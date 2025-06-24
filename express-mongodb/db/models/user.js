f33651042101a121d8ebe843bc2f579de8d567da
jhonp97 <perezjhon2010@alumnos.cei.es>
2025-06-24 23:58:25 +0200

    actualizado

--- express-mongodb/db/models/user.js
+++ express-mongodb/db/models/user.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
 const options = {
     collection: "users", // nombre de la coleccion en MongoDB
@@ -33,17 +32,5 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     }
 },  options );
-
-//  hashear la contraseña antes de guardar
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-//  Método para comparar contraseñas
-userSchema.methods.comparePassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
 
 export const User = mongoose.model("User", userSchema);
