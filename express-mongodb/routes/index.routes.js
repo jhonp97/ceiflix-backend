aac266c8ddc9d9927e8631212a525a3f7b1733e0
jhonp97 <perezjhon2010@alumnos.cei.es>
2025-06-24 22:34:21 +0200

    act

--- express-mongodb/routes/index.routes.js
+++ express-mongodb/routes/index.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import { getWatchedMovies } from "../controllers/movie.controller.js";
+import movieRoutes from "./movies.routes.js";
 
 const router = express.Router();
 
 router.get("/mylist", authMiddleware, getWatchedMovies);
+router.use("/auth", authRoutes);
+router.use("/peliculas", movieRoutes);
 
+export { router };
